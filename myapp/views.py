1c06d416262ead6e35eca2219f73a51ca9b08bbc
mithu <nilchirkutpoison@gmail.com>
2023-10-05 23:24:11 +0600

    image added

---
+++ myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Image
+from .forms import ImageForm
+# Create your views here.
+def home(request):
+    if request.method == 'POST':
+        fm = ImageForm(request.POST, request.FILES)
+        
+        if fm.is_valid():
+            fm.save()
+    else:
+        fm = ImageForm()
+    img = Image.objects.all()
+    return render(request, 'myapp/home.html', {'img':img, 'form':fm})
